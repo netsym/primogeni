e433513886e3207a2b02d0ed4d585c6382a0ae5a
Mohammad Abu Obaida <obaida007@gmail.com>
2014-10-01 15:26:10 -0400

    Update startVEs.pl
    Create Container with openvz,  new template tar file

--- netscript/src/monitor_scripts/startVEs.pl
+++ netscript/src/monitor_scripts/startVEs.pl
@@ -1,4 +1,5 @@
 #!/usr/bin/perl
+#usage: perl /primex/scripts/createFolders.pl 237 237
 
 use strict;
 my $i;
@@ -6,13 +7,49 @@ my $from = $ARGV[0];
 my $to   = $ARGV[1];
 
 for($i=$from; $i<=$to; $i++){
+   #kill a container: if there is a container with duplicate CT_ID
+   `/usr/sbin/vzctl stop $i`;
+   `/usr/sbin/vzctl destroy $i`;
+
    #Create configuration file before starting the container
-   `cp /etc/vz/conf/template.conf /etc/vz/conf/$i.conf`;
+   #container template should be palced in /vz/tempalte/cache
+   `/usr/sbin/vzctl create $i --ostemplate centos-6-x86_64-minimal --config basic`;
+   if ( $? != 0 )
+   {
+     print "command failed: failed to container VE with id=$i\n";
+     exit 1;
+   }
+   #set onboot yes for container, that will be reflected to /etc/vz/conf/CT_ID.conf
+   `/usr/sbin/vzctl set $i --onboot yes --save`;
    if ( $? != 0 )
    {
      print "command failed: failed to create configuration file for VE with id=$i\n";
      exit 1;
    }  
+   #Setting hostname
+   `/usr/sbin/vzctl set $i --hostname host_1001 --save`;
+   if ( $? != 0 )
+   {
+     print "command failed: failed to set or save hostname for VE with id=$i\n";
+     exit 1;
+   }
+   #Setting nameserver
+   `/usr/sbin/vzctl set $i --nameserver 10.1.0.1 --save`;
+   if ( $? != 0 )
+   {
+     print "command failed: failed to set nameserver VE with id=$i\n";
+     exit 1;
+   }
+   #Setting origin_sample
+   #`/usr/sbin/vzctl set $i --origin_sample vps.basic --save`;
+   #if ( $? != 0 )
+   #{
+   #  print "command failed: failed to set origin_ORIGIN_SAMPLE="name" that is name of container sample configuration which the container is based on. VE with id=$i\n";
+   #  exit 1;
+   #}
+
+
+
 
    #Start the container
    `/usr/sbin/vzctl start $i`;
